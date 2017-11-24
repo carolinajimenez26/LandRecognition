25df719e5a00c82747b1eab2c7bce231e98aa74e
Carolina <krito_j_g@utp.edu.co>
2017-11-23 20:25:47 -0500

    Reading tiff images with tiffio.h

--- readtiff_tiffio.cpp
+++ readtiff_tiffio.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char **argv) {
 	TIFF *image;
 	uint32 width, height;
-	int r1,c1, t1, imagesize;
+	int row, col, imagesize;
 	int nsamples;
 	unsigned char *scanline=NULL;
 
@@ -20,6 +20,7 @@ int main(int argc, char **argv) {
 	uint16 RowPerStrip; // número de filas de cada strip
 	uint16 TileWidth; // numero de columnas en cada tile
 	uint16 TileLength; // numero de filas en cada tile
+	tsize_t scanline_size;
 	image = TIFFOpen(argv[1], "r");
 
 	// Open the TIFF image
@@ -46,25 +47,33 @@ int main(int argc, char **argv) {
 	imagesize = height * width;	//get image size
 	dbg(imagesize);
 
-	// //allocate memory for reading tif image
-	// scanline = (unsigned char *)_TIFFmalloc(SamplesPerPixel*width);
-	// if (scanline == NULL){
-	// 	fprintf (stderr,"Could not allocate memory!\n");
-	// 	exit(0);
-	// }
-  //
-	// for (r1 = 0; r1 < height; r1++) {
-	// 	TIFFReadScanline(image, scanline, r1, 0);
-	// 	for (c1 = 0; c1 < width; c1++) {
-	// 		t1 = c1*SamplesPerPixel;
-  //
-	// 		for(i=0; i<SamplesPerPixel; i++)
-	// 			printf("%u \t", *(scanline + t1+i));
-	// 		printf("\n");
-	// 	}
-	// }
-  //
-	// _TIFFfree(scanline); //free allocate memory
+	//allocate memory for reading tif image
+	// scanline = (unsigned char *)_TIFFmalloc(SamplesPerPixel * width);
+	scanline_size = TIFFScanlineSize(image);
+	scanline = (unsigned char *)_TIFFmalloc(scanline_size);
+	if (scanline == NULL){
+		fprintf (stderr,"Could not allocate memory!\n");
+		exit(0);
+	}
+
+	FILE *f = fopen("tiffio_matrix.out", "w");
+	for (row = 0; row < height; row++) {
+		int n = TIFFReadScanline(image, scanline, row, 0); // gets all the row
+		if(n==-1){
+	    printf("Error");
+	    return 1;
+    }
+		for (col = 0; col < width; col++) {
+			// printf("%d ", scanline[col]);
+			fprintf(f, "%d ", scanline[col]);
+		}
+		// printf("\n");
+		fprintf(f, "\n");
+	}
+
+	fclose(f);
+
+	_TIFFfree(scanline); //free allocate memory
 
 	TIFFClose(image);
 
